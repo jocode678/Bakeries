1c42acdd4277834f7888502bac1d018c190756d5
Jo <jdaniell7272@gmail.com>
2022-04-28 22:18:51 +0100

    added radio buttons for dietary reqs on bakery form

--- cakemeapp/application/service.py
+++ cakemeapp/application/service.py
@@ -17,6 +17,10 @@ def get_all_bakeries():
     return Bakeries.query.all()
 
 
+def get_all_dietary_reqs():
+    return Dietary.query.with_entities(Dietary.category).all()
+
+
 def get_bakery_by_id(bakery_id):
     if bakery_id > 0:
         return Bakeries.query.get(bakery_id)
@@ -30,6 +34,7 @@ def get_customer_by_id(customer_id):
     else:
         return None
 
+
 # def get_team_by_id(team_id):
 #     if team_id < 100:
 #         return Address.query.get(team_id)
@@ -39,3 +44,10 @@ def get_customer_by_id(customer_id):
 def add_new_bakery(bakery):
     db.session.add(bakery)
     db.session.commit()
+
+
+def add_new_customer(customer_member):
+    db.session.add(customer_member)
+    db.session.commit()
+
+
