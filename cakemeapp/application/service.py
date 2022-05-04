622e9e7abe981b479e41720a0a9925f466f30fe8
Jo <jdaniell7272@gmail.com>
2022-05-04 09:23:32 +0100

    removed some print statements

--- cakemeapp/application/service.py
+++ cakemeapp/application/service.py
@@ -24,8 +24,6 @@ def get_all_bakeries():
 def get_all_dietary_reqs():
     return Dietary.query.with_entities(Dietary.category).all()
 
-# print(get_all_dietary_reqs())
-
 
 def get_bakery_by_id(bakery_id):
     if bakery_id > 0:
@@ -97,19 +95,13 @@ def get_address_id_4():
 
 # Using this to get the address into the individual bakery page
 def get_address_for_bakery(bakery_id):
-    if Bakeries.address_ref == bakery_id:
-        # look up address id in the bakery table, don't just assume it will be the same id number
-        # address_id = bakery_id
+    if bakery_id > 0:
+        address_id = bakery_id
         return Address.query.get(address_id)
     else:
         return None
 
-# def xx(bakery_id):
-#     all_bakeries = Bakeries.query.all()
-#     for loop to cycle through until address_ref mathes bakery_id
-
-
-print(get_address_for_bakery(1))
+# CHANGE ABOVE IF TIME - look up address id in the bakery table, don't just assume it will be the same id number
 
 def add_new_customer(customer_member):
     db.session.add(customer_member)
